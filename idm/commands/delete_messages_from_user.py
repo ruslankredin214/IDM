334f4bf067c3514fb126865969b7e986977a5872
ruslankredin214 <62007620+ruslankredin214@users.noreply.github.com>
2020-03-14 22:33:06 +0600

    Update delete_messages_from_user.py

--- idm/commands/delete_messages_from_user.py
+++ idm/commands/delete_messages_from_user.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 @dp.event_handle(dp.Methods.DELETE_MESSAGES_FROM_USER)
 def delete_messages_from_user(event: Event) -> str:
-    message_id = utils.new_message(event.api, event.chat.peer_id, message="... удаляю сообщения ...")
+    message_id = utils.new_message(event.api, event.chat.peer_id, message="удаляю сообщения...")
 
     user_id = event.obj['user_id']
     amount = event.obj.get("amount", None)
@@ -30,5 +30,5 @@ def delete_messages_from_user(event: Event) -> str:
         else:
             utils.edit_message(event.api, event.chat.peer_id, message_id, message=f"❗ Не удалось удалить сообщения. Ошибка VK {e.error_msg}")
     except:
-        utils.edit_message(event.api, event.chat.peer_id, message_id, message=f"❗ Произошла неизвестная ошибка.")
+        utils.edit_message(event.api, event.chat.peer_id, message_id, message=f"❗ Произошла неизвестная ошибка")
     return "ok"
